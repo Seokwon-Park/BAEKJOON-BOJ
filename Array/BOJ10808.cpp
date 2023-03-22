1d99790acafa91345dc6848c49e0d0eb117ea270
박석원 <rty3394@naver.com>
2023-03-22 19:37:28 +0900

    Add files via upload

---
+++ Array/BOJ10808.cpp
@@ -0,0 +1,21 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{ 
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	string S;
+//	cin >> S;
+//	int alpha[26];
+//	fill(alpha, alpha + 26, 0);
+//
+//	for (char c : S)
+//		alpha[c - 'a']++;
+//
+//	for (int i : alpha)
+//		cout << i << " ";
+//
+//	return 0;
+//}
