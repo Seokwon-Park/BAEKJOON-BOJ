78a3b25a9342e681aa3edb4c2fc13cf795fbd586
박석원 <rty3394@naver.com>
2023-04-10 13:23:34 +0900

    Update and rename BOJ1074 to BOJ1074.cpp

--- Recursive/BOJ1074.cpp
+++ Recursive/BOJ1074.cpp
@@ -6,13 +6,13 @@ int func(int n, int r, int c)
 {
 	if (n == 0)return 2*r+c;
 	int half = (1 << n);
-  //1 번째 구역
+  	//1 번째 구역
 	if (r < half && c < half) return func(n - 1, r, c);
-  //2 번째 구역
+	//2 번째 구역
 	else if (r < half && c >= half) return half * half + func(n - 1, r, c - half);
-  //3 번째 구역
+  	//3 번째 구역
 	else if (r >= half && c < half) return 2 * half * half + func(n - 1, r - half, c);
-  //4 번째 구역
+  	//4 번째 구역
 	else return 3 * half * half + func(n - 1, r - half, c - half);
 
 }
