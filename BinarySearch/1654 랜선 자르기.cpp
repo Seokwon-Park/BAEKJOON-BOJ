3be5d981280c3038f38bc3dbc90fbcb8772e33c2
SeokwonPark <rty3394@naver.com>
2023-07-26 13:53:50 +0900

    Create 1654 랜선 자르기.cpp

---
+++ BinarySearch/1654 랜선 자르기.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+//parametric search
+using namespace std;
+
+using ll = long long;
+
+ll arr[10005];
+int k, n;
+
+
+bool solve(ll t)
+{
+	ll sum = 0;
+	for (int i = 0; i < k; i++)
+	{
+		sum += arr[i] / t;
+	}
+
+	return sum >= n;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> k >> n;
+
+	for (int i = 0; i < k; i++)
+	{
+		cin >> arr[i];
+	}
+
+	ll st = 1;
+	ll en = INT_MAX;
+
+	while (st < en)
+	{
+		ll mid = (en + st + 1) / 2;
+		if (solve(mid)) st = mid;
+		else en = mid - 1;
+	}
+	cout << st;
+
+
+	return 0;
+}
