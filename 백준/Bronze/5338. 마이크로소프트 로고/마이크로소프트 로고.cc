b38e6b691e7eb7babe9834e3813067df0060a463
SeokwonPark <rty3394@naver.com>
2023-11-12 23:52:13 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout << "       _.-;;-._\n" <<
+		"'-..-'|   ||   |\n" <<
+		"'-..-'|_.-;;-._|\n"<<
+		"'-..-'|   ||   |\n"<<
+		"'-..-'|_.-''-._| ";
+
+	
+	return 0;
+}
