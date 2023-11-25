5fb33179ac1d86f6ed9c65ad2760f946985574f6
SeokwonPark <rty3394@naver.com>
2023-11-25 23:55:49 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	string a, b;
+	cin >> a >> b;
+
+	if (a.length() >= b.length())
+	{
+		cout << "go";
+	}
+	else
+	{
+		cout << "no";
+	}
+
+	return 0;
+}
