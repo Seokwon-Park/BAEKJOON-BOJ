634f4ce9128fc7bc1ef11d17b2e3ffa7a46fbbce
SeokwonPark <rty3394@naver.com>
2023-12-14 14:17:10 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -1,23 +1,9 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 string solution(string X, string Y) {
     string answer = "";
-    
-    std::sort(X.begin(), X.end());
-    std::sort(Y.begin(), Y.end());
-
-    std::string Z;
-
-    std::set_intersection(X.begin(), X.end(), Y.begin(), Y.end(),
-                          std::back_inserter(Z));
-    
-    if(Z.length() == 0) return "-1";
-       
-    sort(Z.begin(), Z.end(), [](char a, char b){return a> b;});
-    
-    if(count(Z.begin(), Z.end(), '0') == Z.length()) return "0";
-        
-    return Z;
+    return answer;
 }
