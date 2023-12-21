703c6e0f3f1d4d956be852e1265dbafc4c8cd8d5
SeokwonPark <rty3394@naver.com>
2023-12-21 14:07:34 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.43 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -19,19 +19,14 @@ long long OddBit(long long n)
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
     
-    // 주어진 정수보다 큰 수부터 +1 씩 다른 비트의 개수가 2개 이하인 수를 찾아라
-    // 짝수: f(x) = x + 1
-    // 홀수: x의 연속된 1의 비트의 개수가 n개라고 할 때, n - 1자리의 비트 수만큼 더해진게 f값이 된다
-    // ex) 101111 의 f값은 000111를 더한 값이 된다
     for (auto& n : numbers) {
         if (n % 2 == 0) { // 짝수
-            answer.push_back(n + 1);
+             answer.push_back(n + 1);
         } else { // 홀수
             long long bit = 1;
-            // 가장 오른쪽부터 차례로 n 개의 연속된 1 로 이루어진 비트구하기
-            while (true) {
-                if ((n & bit) == 0) break;
-                bit *= 2; // 곱하기 2 를 하면 다음 비트로
+            while ((n & bit) != 0)
+            {
+                bit *= 2; 
             }
             bit = bit / 2;
             answer.push_back(n + bit);
--- 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 2개 이하로 다른 비트 - 77885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885) 
 
 ### 성능 요약
 
-메모리: 27.9 MB, 시간: 30.32 ms
+메모리: 27.7 MB, 시간: 30.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 14:5:58
+2023년 12월 4일 14:7:33
 
 ### 문제 설명
 
