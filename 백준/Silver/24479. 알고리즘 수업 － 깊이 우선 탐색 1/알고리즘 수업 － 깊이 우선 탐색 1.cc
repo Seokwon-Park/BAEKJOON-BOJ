1736e61c9048322de6d71f0fde4f25210993830d
SeokwonPark <rty3394@naver.com>
2023-12-26 17:21:15 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 116 ms, Memory: 12588 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> adj[100005];
+bool visited[100005];
+
+int seq[100005];
+int s = 1;
+
+void dfs(int pos)
+{
+	seq[pos] = s;
+	s++;
+	sort(adj[pos].begin(), adj[pos].end());
+	for (auto i : adj[pos])
+	{
+		if (visited[i]) continue;
+		visited[i] = true;
+		dfs(i);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m, r;
+	cin >> n >> m >> r;
+
+	for (int i = 0; i < m; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	visited[r] = true; 
+	dfs(r);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << seq[i] << '\n';
+	}
+	
+	return 0;
+}
