082e3e29c662b96843a0240eb90004cc55c1ecf4
SeokwonPark <rty3394@naver.com>
2023-12-26 17:28:08 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 108 ms, Memory: 8936 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> adj[100005];
+bool visited[100005];
+
+int seq[100005];
+int s = 1;
+
+int n, m, r;
+
+void bfs(int pos)
+{
+	seq[pos] = s;
+	s++;
+
+	for (int i = 0; i <= n; i++)
+	{
+		sort(adj[i].rbegin(), adj[i].rend());
+	}
+	
+	queue<int> q;
+	q.push(pos);
+	while (!q.empty())
+	{
+		int fr = q.front();
+		q.pop();
+		for (int i : adj[fr])
+		{
+			if (visited[i]) continue;
+			q.push(i);
+			visited[i] = true;
+			seq[i] = s++;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	cin >> n >> m >> r;
+
+	for (int i = 0; i < m; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	visited[r] = true; 
+	bfs(r);
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << seq[i] << '\n';
+	}
+	
+	return 0;
+}
