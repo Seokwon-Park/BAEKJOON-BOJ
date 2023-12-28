2ba21c20c6462d79d89158abac5cc75119f2be2d
SeokwonPark <rty3394@naver.com>
2023-12-28 16:59:39 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -1,12 +1,6 @@
-#include <bits/stdc++.h>
-
 using namespace std;
 
 long long solution(int w,int h) {
     long long answer = 1;
-    
-    long long lw = w;
-    long long lh = h;
-    answer = lw*lh - (lw+lh-gcd(lh,lw));
     return answer;
 }
