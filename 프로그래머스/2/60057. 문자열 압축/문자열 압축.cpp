2f431b48b7b7d5a9bffce48dc62da5cf469ae5d3
SeokwonPark <rty3394@naver.com>
2024-01-09 17:17:42 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.cpp
@@ -6,37 +6,61 @@ int solution(string s) {
     int answer = INT_MAX;    
     
     int n = s.length();
-    for(int i =1; i <= n; i ++)
+    if(n ==1)
+    {
+        return 1;
+    }
+    for(int i =1; i <= n/2; i ++)
     {
         int cnt = 0;
         int len = 0;
         string pattern;
+        string newstr = "";
+        string prev_pattern = s.substr(0,i);
         for(int j =0; j< n ;)
         {
             pattern = s.substr(j,i);
-            if(s.find(pattern,j) != -1)
+            if(pattern == prev_pattern) // 이전패턴과 같으면 개수 추가
             {
-                cout << s.find(pattern,j) << ' ' << pattern << '\n';
+                //cout << s.find(pattern,j) << ' ' << pattern << '\n';
                 cnt++;
                 j += pattern.length();
             }
-            else
+            else // 아닌경우
             {
-                pattern = s.substr(j,i);
                 if(cnt > 1)
                 {
-                    len+=2;   
+                    newstr += to_string(cnt);
+                    newstr += prev_pattern; 
+                    cnt = 0; 
+                    len+=2; 
                 }
                 else
                 {
+                    newstr += prev_pattern;
+                    cnt= 0;
                     len +=1;
                 }
-            }           
+                prev_pattern = s.substr(j,i);  
+                
+            }                  
         }
-        cout << len;
-        if(len < answer)
+        if(cnt > 1)
         {
-            answer = len;
+            newstr += to_string(cnt);
+            newstr += prev_pattern; 
+            cnt = 0; 
+            len+=2; 
+        }
+        else
+        {
+            newstr += prev_pattern;
+            cnt= 0;
+            len +=1;
+        }
+        if(newstr.length() < answer)
+        {
+            answer = newstr.length();
         }
     }
 
