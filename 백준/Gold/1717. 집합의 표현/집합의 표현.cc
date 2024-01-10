1e2524df949b357d3c476624e18ead77838c3512
SeokwonPark <rty3394@naver.com>
2024-01-10 15:48:43 +0900

    [Gold V] Title: 집합의 표현, Time: 40 ms, Memory: 6324 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int p[1000001];
+//int board[1005][1005];
+//bool ismst[1005];
+
+int find(int x)
+{
+	if (x == p[x]) return x;
+	return p[x] = find(p[x]);
+}
+
+void yunion(int u, int v)
+{
+	u = find(u);
+	v = find(v);
+	if (u == v) return;
+	//union by rank
+	if (p[u] < p[v]) // u의 parent가 더 높은 위치인 경우
+	{
+		p[v] = u;
+	}
+	else
+	{
+		p[u] = v;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i <= n; i++) p[i] = i;
+
+	for (int i = 0; i < m; i++)
+	{
+		int op, a, b;
+		cin >> op >> a >> b;
+		if (op == 0)
+		{
+			yunion(a, b);
+		}
+		else
+		{
+			cout << (find(a) == find(b) ? "YES" : "NO") << '\n';
+		}
+	}
+	//fill(p, p + 1005, -1);
+	
+
+	return 0;
+}
