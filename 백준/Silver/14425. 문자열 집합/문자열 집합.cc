2adb9abb7036819eeedaf433c256020e36f4f7d0
SeokwonPark <rty3394@naver.com>
2024-02-01 14:20:54 +0900

    [Silver IV] Title: 문자열 집합, Time: 84 ms, Memory: 7944 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m, answer = 0;
+	cin >> n >> m;
+
+	set<string> s;
+
+	for (int i = 0; i< n; i++)
+	{
+		string input;
+		cin >> input;
+		s.insert(input);
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		string input;
+		cin >> input;
+		if (s.find(input) != s.end())
+		{
+			answer++;
+		}
+	}
+
+	cout << answer;
+	return 0;
+}
