e391db6e49329684d4909163f85b34bceb2072c4
SeokwonPark <rty3394@naver.com>
2024-03-12 02:01:22 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, b;
+	while (cin >> a >> b)
+	{
+		cout << b / (a + 1) << '\n';
+	}
+
+
+	return 0;
+}
