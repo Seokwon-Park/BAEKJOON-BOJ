b4976ad29bbc0f428a29d1e213282cce862f2536
SeokwonPark <rty3394@naver.com>
2024-03-12 02:03:40 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout <<
+		"     /~\\"<< '\n' <<
+		"    ( oo|" << '\n' <<
+		"    _\\=/_" << '\n' <<
+		"   /  _  \\" << '\n' <<
+		"  //|/.\\|\\\\" << '\n' <<
+		" ||  \\ /  ||" << '\n' <<
+		"============" << '\n' <<
+		"|          |" << '\n' <<
+		"|          |" << '\n' <<
+		"|          |";
+
+
+	return 0;
+}
