ee760ee044180ecd8d6dca0a8d67ad99017ab4d7
SeokwonPark <rty3394@naver.com>
2024-03-12 02:02:21 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout <<
+		".  .   ." << '\n' <<
+		"|  | _ | _. _ ._ _  _" << '\n' <<
+		"|/\\|(/.|(_.(_)[ | )(/.";
+
+
+	return 0;
+}
