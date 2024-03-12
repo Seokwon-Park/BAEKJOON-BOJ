a0ce1ffbe636d7a01754a59f9752b98a11a5c16c
SeokwonPark <rty3394@naver.com>
2024-03-12 18:24:33 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n, a, b;
+	cin >> n >> a >> b;
+	if (a == b) cout << "Anything";
+	else if (a < b) cout << "Bus";
+	else cout << "Subway";
+
+	return 0;
+}
