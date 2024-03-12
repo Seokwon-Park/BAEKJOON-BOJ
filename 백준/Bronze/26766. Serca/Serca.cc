8750aad699a767225fc2c7e6aeb81af8e1dbe2a3
SeokwonPark <rty3394@naver.com>
2024-03-12 17:58:01 +0900

    [Bronze V] Title: Serca, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		cout <<
+			" @@@   @@@ " << '\n' <<
+			"@   @ @   @" << '\n' <<
+			"@    @    @" << '\n' <<
+			"@         @" << '\n' <<
+			" @       @ " << '\n' <<
+			"  @     @  " << '\n' <<
+			"   @   @   " << '\n' <<
+			"    @ @    " << '\n' <<
+			"     @     " << '\n';
+	}
+	return 0;
+}
