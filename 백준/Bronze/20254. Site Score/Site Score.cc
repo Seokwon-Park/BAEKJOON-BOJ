32c1504b168221396f558780dc578f84c7202774
SeokwonPark <rty3394@naver.com>
2024-03-12 19:34:19 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	cout << a * 56 + b * 24 + c * 14 + d * 6;
+
+	return 0;
+}
