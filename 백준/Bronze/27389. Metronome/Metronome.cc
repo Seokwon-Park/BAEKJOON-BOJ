cc9cb6bbf9b3188216db0def47cf893f006d174f
SeokwonPark <rty3394@naver.com>
2024-03-12 17:46:45 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	double n;
+	cin >> n;
+	cout << fixed;
+	cout.precision(2);
+	cout << n / 4;
+
+	return 0;
+}
