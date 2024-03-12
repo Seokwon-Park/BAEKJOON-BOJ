2a0f475d255736d11f97786da69db29efd35b9ad
SeokwonPark <rty3394@naver.com>
2024-03-12 20:30:21 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	cout << n * 78 / 100 << ' ' << n - n / 5 * 22 / 100;
+
+
+	return 0;
+}
