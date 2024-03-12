1b8f33244a2a1c1ac51b62583f525548ccadd983
SeokwonPark <rty3394@naver.com>
2024-03-12 18:40:20 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	cout << (n ? "Leading the Way to the Future" : "YONSEI");
+
+	return 0;
+}
