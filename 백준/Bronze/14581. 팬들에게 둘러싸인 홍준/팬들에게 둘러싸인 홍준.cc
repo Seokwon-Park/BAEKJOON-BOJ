7f0f80d0b48e8ea69c637b5c87f3c169a9b22020
SeokwonPark <rty3394@naver.com>
2024-03-12 18:57:49 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string fan = ":fan:";
+	string id;
+	cin >> id;
+	id = ':' + id + ':';
+	cout << fan << fan << fan << '\n';
+	cout << fan << id << fan << '\n';
+	cout << fan << fan << fan << '\n';
+	
+
+	return 0;
+}
