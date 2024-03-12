73d30656435cc0a8a9949c4b8435d55f551b20db
SeokwonPark <rty3394@naver.com>
2024-03-12 17:52:56 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int a, b;
+	cin >> a >> b;
+	if (a < b)cout << -1;
+	else if (a == b) cout << 0;
+	else cout << 1;
+
+	return 0;
+}
