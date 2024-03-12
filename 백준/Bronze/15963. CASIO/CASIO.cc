a8a82acefcdd8c630de6ca3361f398a8c7ba3b88
SeokwonPark <rty3394@naver.com>
2024-03-12 19:05:22 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	long long n, m;
+	cin >> n >> m;
+
+	cout << (n == m);
+
+
+	return 0;
+}
