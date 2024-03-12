f4488a64c777246354d3fe0f891053385b8b24d1
SeokwonPark <rty3394@naver.com>
2024-03-12 18:26:12 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		cout << n << ' ' << n << '\n';
+	}
+
+	return 0;
+}
