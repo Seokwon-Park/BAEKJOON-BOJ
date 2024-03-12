b536aba031427c9ad1df0fc0baf58ab73138f36e
SeokwonPark <rty3394@naver.com>
2024-03-12 20:38:32 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int ca = 1;
+	while (true)
+	{
+		int n;
+		cin >> n;
+		if (n == 0) break;
+		for (int i = 0; i < n; i++)
+		{
+			int input;
+			cin >> input;
+		}
+		cout << "Case "<< ca++ << ": Sorting... done!\n";
+	}
+
+	return 0;
+}
