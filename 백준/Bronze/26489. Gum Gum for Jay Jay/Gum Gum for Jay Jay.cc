d5513be4516da18fa672a747b3ca0d539ae60c76
SeokwonPark <rty3394@naver.com>
2024-03-12 17:48:53 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	string s;
+	int cnt = 0;
+	while (getline(cin, s))
+	{
+		cnt++;
+	}
+	cout << cnt;
+
+	return 0;
+}
