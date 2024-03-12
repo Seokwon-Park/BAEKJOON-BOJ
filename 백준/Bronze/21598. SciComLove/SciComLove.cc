df58aab845e2373a29748e37f87a378667fa1095
SeokwonPark <rty3394@naver.com>
2024-03-12 20:33:25 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	while (n--)
+		cout << "SciComLove\n";
+
+	return 0;
+}
