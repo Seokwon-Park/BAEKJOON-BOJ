74bf0c202ae4892edabab113e6c6c4097f38dfa6
SeokwonPark <rty3394@naver.com>
2024-03-12 15:40:58 +0900

    [Bronze V] Title: Everyone is a winner, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int m, k;
+	cin >> m >> k;
+	cout << (m % k == 0 ? "Yes" : "No");
+
+
+	return 0;
+}
