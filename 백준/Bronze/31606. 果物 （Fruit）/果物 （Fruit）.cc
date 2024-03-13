520d5e1219dee19b760fadea8639d62e13440ad4
SeokwonPark <rty3394@naver.com>
2024-03-13 15:32:10 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int x, y;
+	cin >> x >> y;
+	cout << x + y + 3;
+
+
+	return 0;
+}
