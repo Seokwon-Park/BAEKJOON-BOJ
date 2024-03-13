8b0c94e3897d26cc94c62d1a3879a31e5c573096
SeokwonPark <rty3394@naver.com>
2024-03-13 14:31:50 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	char c;
+	cin >> c;
+	if (c == 'n' || c == 'N')
+		cout << "Naver D2";
+	else
+		cout << "Naver Whale";
+
+
+	return 0;
+}
