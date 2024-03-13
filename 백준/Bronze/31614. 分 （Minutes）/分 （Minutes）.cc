1058c257a92f1458db356ce3ce4d63386d455dbf
SeokwonPark <rty3394@naver.com>
2024-03-13 15:34:48 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int h, m;
+	cin >> h >> m;
+	cout << h * 60 + m;
+
+	return 0;
+}
