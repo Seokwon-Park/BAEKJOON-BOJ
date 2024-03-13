006842daed05076b219fc7eefeead3fdb54efa54
SeokwonPark <rty3394@naver.com>
2024-03-13 19:35:10 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int r, c;
+	cin >> r >> c;
+
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+			cout << '*';
+		cout << '\n';
+	}
+
+	return 0;
+}
