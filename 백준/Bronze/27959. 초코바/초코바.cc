900a5a0e00129881ce78f09e51fbb9495a10f1b2
SeokwonPark <rty3394@naver.com>
2024-03-13 14:04:19 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n, m;
+	cin >> n >> m;
+	cout << (n * 100 >= m ? "Yes" : "No");
+
+	return 0;
+}
