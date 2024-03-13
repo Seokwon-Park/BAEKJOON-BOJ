d62c3591e13f6b3dd67a9b682da5cacb3b62d813
SeokwonPark <rty3394@naver.com>
2024-03-13 14:26:04 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int r, s;
+	cin >> r >> s;
+	cout << r * 8 + s * 3 - 28;
+	
+	return 0;
+}
