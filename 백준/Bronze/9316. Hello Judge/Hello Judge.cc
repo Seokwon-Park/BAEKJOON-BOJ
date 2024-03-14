8fa82352d14269c5dac24d1262f31c4bda53c496
SeokwonPark <rty3394@naver.com>
2024-03-14 20:46:34 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int tc;
+	cin >> tc;
+	for (int i = 1; i <= tc; i++)
+	{
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+
+	return 0;
+}
