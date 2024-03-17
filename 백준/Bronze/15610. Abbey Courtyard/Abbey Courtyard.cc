49ff899664878a38fd66ec2fd1c6bece90080b28
SeokwonPark <rty3394@naver.com>
2024-03-17 18:38:10 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vi = vector<int>;
+using vd = vector<double>;
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	double n;
+	cin >> n;
+	double sq = sqrt(n);
+	cout << fixed;
+	cout.precision(6);
+	cout << sq * 4;
+
+
+	return 0;
+}
