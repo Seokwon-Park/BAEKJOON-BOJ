726c42d69673b3bc8cdbae55bd62e67004eff359
SeokwonPark <rty3394@naver.com>
2024-03-17 21:31:14 +0900

    [Bronze II] Title: 16진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vi = vector<int>;
+using vd = vector<double>;
+using ll = long long;
+using vll = vector<ll>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> hex >> n;
+	cout << dec << n;
+
+	return 0;
+}
