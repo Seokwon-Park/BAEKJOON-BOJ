2ba746ed5accbf97d2d16649464a81c531290a32
SeokwonPark <rty3394@naver.com>
2024-03-17 19:10:30 +0900

    [Bronze III] Title: Crowing, Time: 0 ms, Memory: 1408 KB -BaekjoonHub

---
+++ 백준/Bronze/2321. Crowing/Crowing.f
@@ -0,0 +1,9 @@
+program main
+	implicit none
+	print '(A)', ',~'
+	print '(A)', '( 9> //)'
+	print '(A)', ' )(_///'
+	print '(A)', '( \_>/'
+	print '(A)', ' \__/'
+	print '(A)', '  ^^'
+end program main
