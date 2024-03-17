66c6d33f65a73602bb2c84b880ad170bd2757b18
SeokwonPark <rty3394@naver.com>
2024-03-17 18:39:38 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vi = vector<int>;
+using vd = vector<double>;
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	ll n, m;
+	cin >> n >> m;
+	cout << n * m / (ll)2;
+
+
+	return 0;
+}
