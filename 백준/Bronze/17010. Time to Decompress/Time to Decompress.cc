5f58d4f04f2200d0d771909cfe1ca9834c2ea6e9
SeokwonPark <rty3394@naver.com>
2024-03-18 15:14:40 +0900

    [Bronze IV] Title: Time to Decompress, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vi = vector<int>;
+using vd = vector<double>;
+using ll = long long;
+using vll = vector<ll>;
+template <typename T>
+using vec = vector<T>;
+
+const double PI = acos(-1);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		char c;
+		cin >> n >> c;
+		for (int i = 0; i < n; i++)
+		{
+			cout << c;
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
