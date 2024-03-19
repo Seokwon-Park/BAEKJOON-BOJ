b5af33e354fe7cd8d8fd315a61878dbd48f20afa
SeokwonPark <rty3394@naver.com>
2024-03-19 19:53:04 +0900

    [Bronze IV] Title: Counting Clauses, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vi = vector<int>;
+using vd = vector<double>;
+using ll = long long;
+using vll = vector<ll>;
+template <typename T>
+using vec = vector<T>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m;
+	cin >> m >> n;
+	for (int i = 0; i < m; i++)
+	{
+		int x1, x2, x3;
+		cin >> x1 >> x2 >> x3;
+	}
+	if (m < 8) cout << "unsatisfactory";
+	else cout << "satisfactory";
+
+	return 0;
+}
