b5a3d1ed12869be5be1b3998ffb5b0b7da39b24a
SeokwonPark <rty3394@naver.com>
2024-03-23 23:27:17 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 0 ms, Memory: 2404 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+char board[105][105];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	cout << s.substr(s.size() - 5);
+
+	return 0;
+}
