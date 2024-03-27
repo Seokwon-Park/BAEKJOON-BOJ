f4d82b951d09b8c531ed026b75063c24aaa35ef5
SeokwonPark <rty3394@naver.com>
2024-03-27 12:33:09 +0900

    [Bronze IV] Title: НАМИСЛИХ СИ ЧИСЛО, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31261. НАМИСЛИХ СИ ЧИСЛО/НАМИСЛИХ СИ ЧИСЛО.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+	cout << ((b + a) * a + a) * a;
+
+	return 0;
+}
