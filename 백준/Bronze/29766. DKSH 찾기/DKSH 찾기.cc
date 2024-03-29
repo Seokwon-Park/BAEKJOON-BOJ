f81b24229c4c90cdf3bd97b49517cd63ff8d7c6c
SeokwonPark <rty3394@naver.com>
2024-03-29 15:21:27 +0900

    [Bronze IV] Title: DKSH 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+vector<int> kmp(string s)
+{
+	int j = 0;
+	vector<int> f(s.size());
+	for (int i = 1; i < s.size(); i++)
+	{
+		while (j > 0 && s[i] != s[j]) j = f[j - 1];
+		if (s[i] == s[j]) f[i] = ++j;
+	}
+	return f;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string p = "DKSH";
+	vector<int> f = kmp(p);
+	string s;
+	cin >> s;
+	int cnt = 0;
+	int j = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		while (j > 0 && s[i] != p[j]) j = f[j - 1];
+		if (s[i] == p[j]) j++;
+		if (j == p.size())
+		{
+			cnt++;
+			j = 0;
+		}
+	}
+	cout << cnt;
+
+	return 0;
+}
