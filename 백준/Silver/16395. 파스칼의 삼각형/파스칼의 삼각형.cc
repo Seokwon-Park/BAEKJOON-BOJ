84ce9bf0534880f7f2713a2e280a53c55f3b4fd7
SeokwonPark <rty3394@naver.com>
2024-04-01 22:40:38 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int tri[31][31];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j == 1 || j == i)
+				tri[i][j] = 1;
+			else
+			{
+				tri[i][j] = tri[i - 1][j] + tri[i - 1][j - 1];
+			}
+		}
+	}
+	cout << tri[n][k];
+
+	return 0;
+}
