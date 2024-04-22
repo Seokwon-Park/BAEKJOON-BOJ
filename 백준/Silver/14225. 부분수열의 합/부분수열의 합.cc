009489a657c539ae4da39cc4007f736416a6dad8
SeokwonPark <rty3394@naver.com>
2024-04-22 15:38:09 +0900

    [Silver I] Title: 부분수열의 합, Time: 236 ms, Memory: 22220 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -6,42 +6,37 @@ using ll = long long;
 using ull = unsigned long long;
 using pii = pair<int, int>;
 
+int v[22];
+set<int> s;
+int n;
+
+void func(int k, int m)
+{
+	if (k == n)
+	{
+		s.insert(m);
+		return;
+	}
+
+	func(k + 1, m);
+	func(k + 1, m + v[k]);
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	int n;
 	cin >> n;
-	vector<int> v;
 
 	for (int i = 0; i < n; i++)
 	{
 		int m;
 		cin >> m;
-		v.push_back(m);
+		v[i] = m;
 	}
-	sort(v.begin(), v.end());
 	
-	set<int> s;
-
-	for (int i = 1; i <= n; i++)
-	{
-		vector<int> c(n, 0);
-		fill(c.end() - i, c.end(), 1);
-		do
-		{
-			int res = 0;
-			for (int i = 0; i < n; i++)
-			{
-				if (c[i] == 1)
-				{
-					res += v[i];
-				}
-			}
-			s.insert(res);
-		} while (next_permutation(c.begin(), c.end()));
-	}
+	func(0, 0);
 
 	int k = 1;
 	while (s.find(k) != s.end())
