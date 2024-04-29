ba5d781c3399b6013249c375235980e5741c7fd9
SeokwonPark <rty3394@naver.com>
2024-04-29 16:03:08 +0900

    [Silver I] Title: 귀여운 라이언, Time: 64 ms, Memory: 9108 KB -BaekjoonHub

---
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(n);
+	vector<int> ryan;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+		if (v[i] == 1)
+			ryan.push_back(i);
+	}
+
+	int st = 0;
+	int ed = k - 1;
+	int ans = INT_MAX;
+	if (ryan.size() < k)
+	{
+		cout << -1;
+		return 0;
+	}
+	else
+	{
+		while (ed < ryan.size())
+		{
+			int res = ryan[ed] - ryan[st] + 1;
+			ans = min(ans, res);
+			ed++;
+			st++;
+		}
+		cout << ans;
+	}
+
+	return 0;
+}
