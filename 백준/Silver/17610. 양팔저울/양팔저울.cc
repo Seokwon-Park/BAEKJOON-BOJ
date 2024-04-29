f73365b338df05ed381a98c6eb5d844f2fe3eb04
SeokwonPark <rty3394@naver.com>
2024-04-29 18:02:29 +0900

    [Silver I] Title: 양팔저울, Time: 700 ms, Memory: 106300 KB -BaekjoonHub

--- 백준/Silver/17610. 양팔저울/양팔저울.cc
+++ 백준/Silver/17610. 양팔저울/양팔저울.cc
@@ -9,34 +9,18 @@ using pii = pair<int, int>;
 int v[14];
 int isused[14];
 int n;
-unordered_map<int, int> m;
+unordered_map<int, bool> m;
 
 void func(int k, int num)
 {
-	if (k > 0) 
-	{
-		if (m.find(num) != m.end())
-		{
-			if (m[num] <= k) return;
-			m[num] = min(m[num], k);
-		}
-		else
-			m[num] = k;
-	}
 	if (k == n)
-		return;
-	for (int i = 0; i < n; i++)
 	{
-		if (isused[i]) continue;
-		isused[i] = true;
-		func(k + 1, num + v[i]);
-		if (num < v[i])
-			func(k + 1, v[i] - num);
-		else
-			func(k + 1, num - v[i]);
-		isused[i] = false;
+		m[num] = true;
+		return;
 	}
-
+	func(k + 1, num + v[k]);
+	func(k + 1, num - v[k]);
+	func(k + 1, num);
 }
 
 int main()
@@ -57,12 +41,11 @@ int main()
 	int ans = 0;
 	for (int i = 1; i <= sum; i++)
 	{
-		if (m[i] == 0)
+		if (!m[i])
 			ans++;
 	}
 
 	cout << ans;
-
 
 	return 0;
 }
