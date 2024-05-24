1c52496f3cc95d39ebf3dcd17aecb1d7644491bf
SeokwonPark <rty3394@naver.com>
2024-05-24 12:19:33 +0900

    [Gold IV] Title: 이모티콘, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.cc
+++ 백준/Gold/14226. 이모티콘/이모티콘.cc
@@ -13,21 +13,17 @@ int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-
 	fill(dp, dp + 1005, 0x3f3f3f);
 	int s;
 	cin >> s;
 	dp[1] = 0;
 
-	for (int i = 1; i <= s+1; i++)
+	for (int i = 1; i <= s + 1; i++)
 	{
-		dp[i * 2] = min(dp[i * 2], dp[i] + 2);
-		dp[i * 2 - 1] = min(dp[i * 2 - 1], dp[i * 2] + 1);
-
-		for (int j = i * 3; j <= s * 2; j += i)
+		for (int j = 2; i*j <= s *2; j++)
 		{
-			dp[j] = min(dp[j], dp[i] + j/i);
-			dp[j - 1] = min(dp[j - 1], dp[j] + 1);
+			dp[i * j] = min(dp[i * j], dp[i] + j);
+			dp[i * j - 1] = min(dp[i * j - 1], dp[i * j] + 1);
 		}
 	}
 
