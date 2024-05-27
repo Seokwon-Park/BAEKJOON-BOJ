30694958a365776240f7d58c00f634194b89649e
SeokwonPark <rty3394@naver.com>
2024-05-27 23:54:25 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, ll>;
+const int INF = 0x3f3f3f3f;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, p, c;
+	cin >> a >> p >> c;
+	cout << max(a + c, p);
+	
+	return 0;
+}
