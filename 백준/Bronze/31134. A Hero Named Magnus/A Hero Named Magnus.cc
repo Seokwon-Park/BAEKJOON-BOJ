a039a644dd849da312b0a08567bccb85e919e881
SeokwonPark <rty3394@naver.com>
2024-05-31 17:41:20 +0900

    [Bronze IV] Title: A Hero Named Magnus, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31134. A Hero Named Magnus/A Hero Named Magnus.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, ll>;
+const int INF = 0x3f3f3f3f;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		ll x;
+		cin >> x;
+
+		cout << (x - 1LL) * 2LL + 1LL << '\n';
+	}
+
+
+	return 0;
+}
