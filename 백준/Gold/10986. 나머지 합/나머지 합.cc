d8e60018a5109995b8d5fc382d205dc18afafb89
SeokwonPark <rty3394@naver.com>
2024-06-04 13:40:26 +0900

    [Gold III] Title: 나머지 합, Time: 144 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -8,13 +8,6 @@ using pii = pair<int, int>;
 using tiii = tuple<int, int, ll>;
 const int INF = 0x3f3f3f3f;
 
-ll combination(ll k)
-{
-	if (k == 0) return 0;
-	return k*(k-1LL)/2LL;
-}
-
-
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -24,21 +17,21 @@ int main()
 	cin >> n >> m;
 
 	vector<ll> v(n + 5);
-	vector<ll> pfsum(n + 5);
 	vector<ll> left(n + 5);
 
+	ll pfsum = 0;
 	for (int i = 1; i <= n; i++)
 	{
 		cin >> v[i];
-		pfsum[i] = pfsum[i - 1] + v[i];
-		left[pfsum[i] % m]++;
+		pfsum += v[i];
+		left[pfsum % m]++;
 	}
 
 	ll ans = left[0];
 
 	for (int i = 0; i < m; i++)
 	{
-		ans += combination(left[i]);
+		ans += left[i] * (left[i] - 1LL) / 2LL;
 	}
 
 	cout << ans;
