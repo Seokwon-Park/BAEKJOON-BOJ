7f51090951e8f9706039cdf6e3a48e5223435313
SeokwonPark <rty3394@naver.com>
2024-06-07 18:01:43 +0900

    [Bronze II] Title: 거리의 합, Time: 52 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Bronze/2399. 거리의 합/거리의 합.cc
+++ 백준/Bronze/2399. 거리의 합/거리의 합.cc
@@ -24,19 +24,16 @@ int main()
 	int n;
 	cin >> n;
 	vector<ll> v(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i];
-	}
-
 	ll ans = 0;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = i+1; j < n; j++)
+		cin >> v[i];
+		for (int j = 0; j < i; j++)
 		{
 			ans += my_abs(v[i] - v[j]);
 		}
 	}
+
 	cout << ans*2;
 
 	return 0;
