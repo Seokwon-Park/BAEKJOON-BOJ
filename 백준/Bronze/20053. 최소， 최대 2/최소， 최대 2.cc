bcc113f50889f11fac5ccf62ce4c6d00ce42f614
SeokwonPark <rty3394@naver.com>
2024-06-10 21:48:29 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 344 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, ll>;
+const int INF = 0x3f3f3f3f;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		int mx = -1000000;
+		int mn = 1000000;
+		for (int i = 0; i < n; i++)
+		{
+			int m;
+			cin >> m;
+			mx = max(m, mx);
+			mn = min(m, mn);
+		}
+
+		cout << mn << ' ' << mx << '\n';
+	}
+
+	return 0;
+}
