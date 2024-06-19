4c94a0034fa68b05bcccb5990cd87ba5f16b7cc9
SeokwonPark <rty3394@naver.com>
2024-06-19 20:42:13 +0900

    [Gold V] Title: 루트 게임, Time: 724 ms, Memory: 2996 KB -BaekjoonHub

---
+++ 백준/Gold/16888. 루트 게임/루트 게임.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+
+bool dp[1000005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	for (int i = 1; i * i <= 1000000; i++)
+	{
+		dp[i * i] = 1;
+	}
+
+	for (int i = 1; i <= 1000000; i++)
+	{
+		if (dp[i]) continue;
+		bool checker = true;
+		for (int j = 1; j * j <= i; j++)
+		{
+			checker &= dp[i - j * j];
+		}
+		if (!checker)
+		{
+			dp[i] = 1;
+		}
+	}
+
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+
+		bool isCubeWin = true;
+		for (int i = 1; i * i <= n; i++)
+		{
+			isCubeWin &= dp[n - i * i];
+		}
+		if (isCubeWin)
+			cout << "cubelover";
+		else
+			cout << "koosaga";
+		cout << '\n';
+	}
+	
+
+	return 0;
+}
