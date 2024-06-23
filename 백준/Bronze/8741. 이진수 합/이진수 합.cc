2dc6eeace6beec458e8796a8617553102e21e5db
SeokwonPark <rty3394@naver.com>
2024-06-23 20:30:49 +0900

    [Bronze II] Title: 이진수 합, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8741. 이진수 합/이진수 합.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int k;
+	cin >> k;
+
+	for (int i = 0; i < k; i++)
+		cout << 1;
+	for (int i = 0; i < k - 1; i++)
+		cout << 0;
+
+	return 0;
+}
