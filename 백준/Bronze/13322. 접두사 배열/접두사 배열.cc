c45643ae958c22bb405ee5c2be858e3445ecbebd
SeokwonPark <rty3394@naver.com>
2024-06-26 00:09:08 +0900

    [Bronze I] Title: 접두사 배열, Time: 8 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Bronze/13322. 접두사 배열/접두사 배열.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+
+int main()
+{
+	ios::sync_with_stdio(false);					
+	cin.tie(0);
+	
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		cout << i << '\n';
+	}
+
+
+	return 0;
+}
