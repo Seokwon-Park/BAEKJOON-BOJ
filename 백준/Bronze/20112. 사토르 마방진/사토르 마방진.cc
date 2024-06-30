bedc8ccd3e8c1db6ed69947d6ae733fbe60d13fe
SeokwonPark <rty3394@naver.com>
2024-06-30 23:35:48 +0900

    [Bronze I] Title: 사토르 마방진, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20112. 사토르 마방진/사토르 마방진.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+
+char b[105][105];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> b[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (b[i][j] != b[j][i])
+			{
+				cout << "NO";
+				return 0;
+			}
+		}
+	}
+
+	cout << "YES";
+	return 0;
+}
