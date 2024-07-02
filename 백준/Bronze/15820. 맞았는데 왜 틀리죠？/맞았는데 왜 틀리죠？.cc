23e9fcedd502482107fbbf6dc4940b92fd2cee3c
SeokwonPark <rty3394@naver.com>
2024-07-02 21:45:55 +0900

    [Bronze III] Title: 맞았는데 왜 틀리죠?, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15820. 맞았는데 왜 틀리죠？/맞았는데 왜 틀리죠？.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int s1, s2;
+	cin >> s1 >> s2;
+	for (int i = 0; i < s1; i++)
+	{
+		int ans, sub;
+		cin >> ans >> sub;
+		if (ans != sub)
+		{
+			cout << "Wrong Answer";
+			return 0;
+		}
+	}
+	for (int i = 0; i < s2; i++)
+	{
+		int ans, sub;
+		cin >> ans >> sub;
+		if (ans != sub)
+		{
+			cout << "Why Wrong!!!";
+			return 0;
+		}
+	}
+
+	cout << "Accepted";
+
+	return 0; 
+}
