a5c48f93c764dbef20ab3720c17f4490f6cd5fd9
SeokwonPark <rty3394@naver.com>
2024-07-06 01:42:52 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 8 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int dp[100005];
+int dp2[100005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> v(n+1);
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> v[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		int a = v[i] == 1 ? 1 : -1;
+		dp[i] = max(dp[i - 1] + a,a );
+		int b = v[i] == 2 ? 1 : -1;
+		dp2[i] = max(dp2[i - 1] +b, b);
+	}
+
+	cout << max(*max_element(dp + 1, dp + n + 1), *max_element(dp2 + 1, dp2 + n + 1));
+
+
+	return 0;
+}
