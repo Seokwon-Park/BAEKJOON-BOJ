aeb07cc6f6ea2c34df8eb7b86983930926f66435
SeokwonPark <rty3394@naver.com>
2024-07-09 15:52:21 +0900

    [Bronze III] Title: 2루수 이름이 뭐야, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	string s;
+	while (n--)
+	{
+		cin >> s;
+		if (s == "anj")
+		{
+			cout << "뭐야;";
+			return 0;
+		}
+	}
+	cout << "뭐야?";
+
+	return 0;
+}
