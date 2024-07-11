56b627a219406338c51e07bd2bf755cd5b21e22e
SeokwonPark <rty3394@naver.com>
2024-07-11 20:09:33 +0900

    [Silver V] Title: Costume Party, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/6159. Costume Party/Costume Party.cc
+++ 백준/Silver/6159. Costume Party/Costume Party.cc
@@ -22,16 +22,27 @@ int main()
 		cin >> v[i];
 	}
 
+	sort(v.begin(), v.end());
 	int ans = 0;
-	for (int i = 0; i < n; i++)
+
+	int st = 0;
+	int en = n-1;
+	//1 2 3 5; st en (1, 5)
+	//조건을 만족하는 경우 st보다 크고 en또는 en 이전 인덱스에 있는 녀석들은 st랑 더하면 무조건 조건 만족!
+	//조건을 벗어나는 경우 en의 크기를 줄여준다.
+
+	while (st < en)
 	{
-		for (int j = i + 1; j < n; j++)
+		if (v[st] + v[en] <= s)
 		{
-			if (v[i] + v[j] <= s)
-				ans++;
+			ans += en - st;
+			st++;
+		}
+		else
+		{
+			en--;
 		}
 	}
-
 	cout << ans;
 
 	return 0;
