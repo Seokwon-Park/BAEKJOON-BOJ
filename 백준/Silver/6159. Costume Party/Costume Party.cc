6a3af5d912331b5ab3cb3bb7b59b5e6675cfae66
SeokwonPark <rty3394@naver.com>
2024-07-11 19:23:11 +0900

    [Silver V] Title: Costume Party, Time: 144 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/6159. Costume Party/Costume Party.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n, s;
+	cin >> n >> s;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (v[i] + v[j] <= s)
+				ans++;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
