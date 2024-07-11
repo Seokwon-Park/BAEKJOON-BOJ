fe43b5ea49e58ecb1ca9d41d22b5e24705b9f1df
SeokwonPark <rty3394@naver.com>
2024-07-11 13:02:27 +0900

    [Bronze I] Title: 벼락치기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23739. 벼락치기/벼락치기.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+
+	int cur = 30;
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int t;
+		cin >> t;
+		if (t < cur)
+		{
+			ans++;
+			cur -= t;
+		}
+		else
+		{
+			if (cur * 2 >= t)
+			{
+				ans++;
+			}
+			cur = 30;
+		}
+	}
+	cout << ans;
+
+
+	return 0;
+}
