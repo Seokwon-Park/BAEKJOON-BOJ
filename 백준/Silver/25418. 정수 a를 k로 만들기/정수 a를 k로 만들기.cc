9163276228caacdccbff422a1931ca8977e4d242
SeokwonPark <rty3394@naver.com>
2024-07-16 18:25:44 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.cc
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int dp[1000005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	fill(dp, dp + 1000005, INF);
+	int a, k;
+	cin >> a >> k;
+	dp[a] = 0;
+	for (int i = a; i < k; i++)
+	{
+		if (i * 2 <= k)
+			dp[i * 2] = min(dp[i * 2], dp[i] + 1);
+		dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+	}
+	cout << dp[k];
+
+	return 0;
+}
