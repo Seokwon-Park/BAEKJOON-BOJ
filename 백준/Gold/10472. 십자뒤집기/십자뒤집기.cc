ec9c0042cc5dd609fdd68a1cadb19379b59e3463
SeokwonPark <rty3394@naver.com>
2024-07-17 22:07:27 +0900

    [Gold V] Title: 십자뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10472. 십자뒤집기/십자뒤집기.cc
+++ 백준/Gold/10472. 십자뒤집기/십자뒤집기.cc
@@ -13,39 +13,39 @@ const ll MOD = 1000000007;
 int dx[4] = { -1,0,1,0 };
 int dy[4] = { 0,-1,0,1 };
 
-map<string, bool> v;
+bool v[1 << 10];
 
-string flip(int rc, string s)
+int flip(int rc, int s)
 {
 	int r = rc / 3;
 	int c = rc % 3;
-	s[rc] = s[rc] == '1' ? '0' : '1';
+	s ^= 1 << rc;
 	for (int i = 0; i < 4; i++)
 	{
 		int tx = c + dx[i];
 		int ty = r + dy[i];
 		if (tx < 0 || ty < 0 || tx >= 3 || ty >= 3) continue;
-		s[ty*3+tx] = s[ty * 3 + tx] == '1' ? '0' : '1';
+		s ^= 1 << ty * 3 + tx;
 	}
 	return s;
 }
 
-int bfs(string target)
+int bfs(int target)
 {
-	queue<string> q;
-	q.push(string(9, '0'));
-	v[string(9, '0')] = 1;
+	queue<int> q;
+	q.push(0);
+	v[0] = 1;
 	int clicked = 0;
 	while (!q.empty())
 	{
 		int qs = q.size();
 		for (int j = 0; j < qs; j++)
 		{
-			string cur = q.front();
+			int cur = q.front();
 			q.pop();
 			for (int i = 0; i < 9; i++)
 			{
-				string nxt = flip(i, cur);
+				int nxt = flip(i, cur);
 				if (v[nxt]) continue;
 				if (nxt == target)
 				{
@@ -69,8 +69,8 @@ int main()
 	cin >> tc;
 	while (tc--)
 	{
-		v.clear();
-		string target(9,0);
+		fill(v, v + 1024, false);
+		int target = 0;
 		for (int i = 0; i < 3; i++)
 		{
 			for(int j = 0; j<3; j++)
@@ -78,9 +78,7 @@ int main()
 				char c;
 				cin >> c;
 				if (c == '*')
-					target[i * 3 + j] = '1';
-				else
-					target[i * 3 + j] = '0';
+					target += 1 << (i * 3 + j);
 			}
 		}
 		cout << bfs(target) << '\n';
