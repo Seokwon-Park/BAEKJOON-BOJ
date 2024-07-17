c85144bfe26943b26490c4ffa9c0374b4fd068a0
SeokwonPark <rty3394@naver.com>
2024-07-17 10:51:00 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		ll a, b;
+		cin >> a >> b;
+		cout << (a / b) * (a / b) << '\n';
+	}
+
+	return 0;
+}
