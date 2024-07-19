8c84b139cbc2a78d50d994be77fd6d37721bf5a4
SeokwonPark <rty3394@naver.com>
2024-07-19 14:41:01 +0900

    [Bronze II] Title: 주사위 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5566. 주사위 게임/주사위 게임.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int b[1005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> b[i];
+	}
+
+	int cur = 0;
+	for (int i = 0; i < m; i++)
+	{
+		int cmd;
+		cin >> cmd;
+		cur += cmd;
+		if (cur >= n-1)
+		{
+			cout << i + 1;
+			break;
+		}
+		cur += b[cur];
+		if (cur >= n-1)
+		{
+			cout << i + 1;
+			break;
+		}
+	}
+
+	return 0;
+}
