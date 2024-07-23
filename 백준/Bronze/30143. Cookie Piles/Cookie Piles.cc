b10c44db2efbd10243018599fd9975d960dd1d2d
SeokwonPark <rty3394@naver.com>
2024-07-23 11:57:16 +0900

    [Bronze IV] Title: Cookie Piles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30143. Cookie Piles/Cookie Piles.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n, a, d;
+		cin >> n >> a >> d;
+
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+			ans += a + d * i;
+
+		cout << ans << '\n';
+	}
+
+
+	return 0;
+}
