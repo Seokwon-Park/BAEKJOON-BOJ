ddd00ec006d542bcf3450ecfb7a51470853087ea
SeokwonPark <rty3394@naver.com>
2024-07-26 17:03:12 +0900

    [Bronze III] Title: 접시 안의 원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1000000007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	double n;
+	cin >> n;
+	cout << (int)round((n / 2) * (n / 2));
+
+
+	return 0;
+}
