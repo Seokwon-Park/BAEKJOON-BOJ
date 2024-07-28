196ecd70a8dbd5f731d6b014608f44b8dae1666a
SeokwonPark <rty3394@naver.com>
2024-07-28 20:08:49 +0900

    [Silver IV] Title: 알고리즘 수업 - 피보나치 수 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.cc
+++ 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.cc
@@ -9,6 +9,50 @@ using tiii = tuple<int, int, int>;
 const int INF = 0x3f3f3f3f;
 const ll MOD = 1'000'000'007;
 
+vector<vector<ll>> v
+{
+	{1,1},
+	{1,0}
+};
+
+vector<vector<ll>> multiply(vector<vector<ll>> a, vector<vector<ll>> b)
+{
+	vector<vector<ll>> res(2, vector<ll>(2,0));
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			ll tmp = 0;
+			for (int k = 0; k < 2; k++)
+			{
+				tmp += a[i][k] * b[k][j];
+				tmp %= MOD;
+			}
+			res[i][j] = tmp;
+		}
+	}
+	return res;
+}
+
+vector<vector<ll>> fast_pow(ll n)
+{
+	if (n == 1)
+	{
+		return v;
+	}
+	vector<vector<ll>> res(2, vector<ll>(2, 0));
+	res = fast_pow(n / 2);
+	if (n % 2 == 0)
+	{
+		return multiply(res, res);
+	}
+	else
+	{
+		return multiply(multiply(res, res), v);
+	}
+}
+
+
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -19,15 +63,11 @@ int main()
 
 	ll noRecur = (n - 2) % MOD;
 
-	ll a = 1; 
-	ll b = 1;
-	for (int i = 0; i <= n-3; i++)
-	{
-		ll tmp = (a + b) %MOD;
-		a = b;
-		b = tmp;
-	}
-	cout << b << ' ' << noRecur;
+	
+
+	//f1 = 1 f2 = 1 f3 3 f4 5;
+	//=>f1 1 f2 =1 f3 =2
+	cout << fast_pow(n)[1][0] << ' ' << noRecur;
 
 
 	return 0;
