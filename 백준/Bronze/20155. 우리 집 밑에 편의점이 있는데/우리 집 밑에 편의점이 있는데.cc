d3d85571e0807aa061f818264c564cfb97cf5e04
SeokwonPark <rty3394@naver.com>
2024-07-31 12:05:51 +0900

    [Bronze II] Title: 우리 집 밑에 편의점이 있는데, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20155. 우리 집 밑에 편의점이 있는데/우리 집 밑에 편의점이 있는데.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1'000'000'007;
+
+int v[1005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		v[x]++;
+	}
+
+	cout << *max_element(v + 1, v + 1005);
+	
+
+	return 0;
+}
