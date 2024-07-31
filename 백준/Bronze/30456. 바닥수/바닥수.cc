09e5f00cea5b0160607758a99dd97f0da0918f99
SeokwonPark <rty3394@naver.com>
2024-07-31 12:09:27 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1'000'000'007;
+
+int v[1005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, l;
+	cin >> n >> l;
+	string s = string(l - 1, '1') + to_string(n);
+	cout << s;
+
+	return 0;
+}
