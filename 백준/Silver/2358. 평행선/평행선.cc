26fd3536050cc163fac8a038eb0824d0e22f9b18
SeokwonPark <rty3394@naver.com>
2024-08-02 11:05:45 +0900

    [Silver IV] Title: 평행선, Time: 116 ms, Memory: 13376 KB -BaekjoonHub

---
+++ 백준/Silver/2358. 평행선/평행선.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	map<int, vector<int>> xGroup;
+	map<int, vector<int>> yGroup;
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		xGroup[x].push_back(y);
+		yGroup[y].push_back(x);
+	}
+
+	int ans = 0;
+	for (auto xv : xGroup)
+	{
+		if (xv.second.size() > 1)
+			ans++;
+	}
+	for (auto yv : yGroup)
+	{
+		if (yv.second.size() > 1)
+			ans++;
+	}
+
+	cout << ans;
+	return 0;
+}
