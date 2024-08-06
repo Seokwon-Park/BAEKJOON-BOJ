2795bb9e7439a553233d6e26ff6c190b2a3f526c
SeokwonPark <rty3394@naver.com>
2024-08-06 11:30:35 +0900

    [Silver IV] Title: 수면 장애, Time: 200 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12755. 수면 장애/수면 장애.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1'000'000'007;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	int cur = 0;
+	int len = 0;
+	while (len < n)
+	{
+		cur++;
+		len += to_string(cur).size();
+	}
+
+	len -= n;
+	string tmp = to_string(cur);
+	cout << tmp[tmp.size() - len-1];
+
+	return 0;
+}
