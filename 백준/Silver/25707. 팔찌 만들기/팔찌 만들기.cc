762c7ac090a9d6f74a8f7386e83dd6208ca4b80c
SeokwonPark <rty3394@naver.com>
2024-08-26 16:27:55 +0900

    [Silver V] Title: 팔찌 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1'000'000'007;
+
+vector<bool> isPrint(55, true);
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+	
+	sort(v.begin(), v.end());
+
+	int ans = abs(v[n - 1] - v[0]) * 2;
+	cout << ans;
+
+	return 0;
+}
