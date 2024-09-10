1b06307d111e0ad16176731e239a685607fbc585
SeokwonPark <rty3394@naver.com>
2024-09-10 15:01:52 +0900

    [Silver III] Title: 표절, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2428. 표절/표절.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	ll ans = 0;
+	for (ll i = 1; i < n; i++)
+	{
+		int ix = lower_bound(v.begin(), v.end(), v[i] * 0.9)-v.begin();
+		ans += i - ix;
+	}
+	cout << ans;
+
+	return 0;
+}
