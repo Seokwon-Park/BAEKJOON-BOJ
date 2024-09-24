72000bd971b1921d31fc7269311d2b371c33bad9
SeokwonPark <rty3394@naver.com>
2024-09-24 11:21:17 +0900

    [Silver IV] Title: 피카츄가 낸 어려운 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16488. 피카츄가 낸 어려운 문제/피카츄가 낸 어려운 문제.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	ll n, k;
+	cin >> n >> k;
+	cout << n * n * k;
+
+	return 0;
+}
+
