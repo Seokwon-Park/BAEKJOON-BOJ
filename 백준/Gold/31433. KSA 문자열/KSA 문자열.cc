c58d28184942682cc868328280801881e4187c61
SeokwonPark <rty3394@naver.com>
2024-09-27 10:39:18 +0900

    [Gold IV] Title: KSA 문자열, Time: 8 ms, Memory: 3148 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
+++ 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
@@ -1,76 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	string ksa;
-	cin >> ksa;
-
-	int len = ksa.length();
-	int curlen = len;
+	string s;
+	cin >> s;
 
 	string p = "KSA";
-	int pt = 0;
 
-	int k = 0, s = 0, a = 0;
-
-	for (int i = 0; i < ksa.length(); i++)
+	int ans = INF;
+	for (int i = 0; i < p.size(); i++)
 	{
-		if (ksa[i] == p[pt])
+		int res = i; // 앞에 붙인 값의 길이 "", "K", "KS"
+		string str = p.substr(0, i) + s; // 붙인 문자열
+		int pix = 0; // 패턴의 시작 위치
+		int remove = 0; // KSA 순으로 탐색했을때 삭제해야할 문자 개수
+		for (int j = 0; j < str.size(); j++)
 		{
-			pt = (pt + 1) % 3;
+			if (p[pix] != str[j])
+			{
+				remove++;
+			}
+			else
+			{
+				pix = (pix + 1) % 3;
+			}
 		}
-		else
-		{
-			k++;
-			curlen--; //현재 길이를 지운다.
-		}
+		int resLen = str.size() - remove;// 삭제할거 다지우면 길이 얼마임?
+		int needToModify = abs((int)s.size() - resLen);
+		ans = min(ans, res + remove + needToModify);
+
 	}
-	k += len - curlen; //채워야 하는 길이만큼 뒤에다가 ksa 반복해주면 됨
-
-	curlen = len;
-	pt = 1;
-	for (int i = 0; i < ksa.length(); i++)
-	{
-		if (ksa[i] == p[pt])
-		{
-			pt = (pt + 1) % 3;
-		}
-		else
-		{
-			s++;
-			curlen--; //현재 길이를 지운다.
-		}
-	}
-	s += 1; // s앞에 k를 붙여야 되니까 len도 같이 더해준다.
-	curlen += 1;
-	s += len - curlen;
-
-	curlen = len;
-	pt = 2;
-	for (int i = 0; i < ksa.length(); i++)
-	{
-		if (ksa[i] == p[pt])
-		{
-			pt = (pt + 1) % 3;
-		}
-		else
-		{
-			a++;
-			curlen--; //현재 길이를 지운다.
-		}
-	}
-	a += 2; // a앞에 k,s를 붙여야 하니까.
-	curlen += 2;
-	a += len - curlen;
-
-
-	cout << min({ k,s,a });
-
+	cout << ans;
 
 	return 0;
 }
+
