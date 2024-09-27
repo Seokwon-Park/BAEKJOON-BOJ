180fa3c83ceda171f4613d771dd62c79401ee2ac
SeokwonPark <rty3394@naver.com>
2024-09-27 13:20:28 +0900

    [Silver I] Title: 부동산 다툼, Time: 452 ms, Memory: 11396 KB -BaekjoonHub

---
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.cc
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+set<int> hasMaster;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, q;
+	cin >> n >> q;
+	for (int i = 0; i < q; i++)
+	{
+		int x;
+		cin >> x;
+		int tmp = x;
+		bool isArrive = true;
+		int lastFind = -1;
+		while (tmp)
+		{
+			if (hasMaster.find(tmp) != hasMaster.end())
+			{
+				lastFind = tmp;
+				isArrive = false;
+			}
+			tmp >>= 1;
+		}
+		if (isArrive) // 원하는 땅에 도착할 수 있는 경우 땅 목록에 입력
+		{
+			hasMaster.insert(x);
+			cout << 0 << '\n';
+		}
+		else
+		{
+			cout << lastFind << '\n';
+		}
+	}
+
+
+
+	return 0;
+}
+
