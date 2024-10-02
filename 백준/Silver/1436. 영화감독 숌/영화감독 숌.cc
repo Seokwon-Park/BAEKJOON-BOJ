b033464aa059123649010df5aa3fc4765a51d63d
SeokwonPark <rty3394@naver.com>
2024-10-02 20:16:49 +0900

    [Silver V] Title: 영화감독 숌, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+
+	int i = 666;
+	
+	int ix = 0;
+	while (ix != n)
+	{
+		int tmp = i;
+		while (tmp > 100)
+		{
+			if (tmp % 1000 == 666)
+			{
+				ix++;
+				break;
+			}
+			else
+			{
+				tmp /= 10;
+			}
+		}
+		if(ix != n)	
+			i++;
+	}
+
+	cout << i;
+	return 0;
+}
