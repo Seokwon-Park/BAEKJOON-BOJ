bbc6dfeb0a5a8f2747dea77778321865c702e809
SeokwonPark <rty3394@naver.com>
2024-10-07 15:16:28 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1000000007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	double d;
+	cin >> d;
+	cout << static_cast<int>(d);
+
+
+	return 0;
+}
+
