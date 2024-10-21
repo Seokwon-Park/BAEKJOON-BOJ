7f93c5805c221eec63ec0cb696983dd6bd4a0f65
SeokwonPark <rty3394@naver.com>
2024-10-21 18:31:53 +0900

    [Bronze II] Title: N! mod P (1), Time: 728 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
@@ -1,3 +1,4 @@
+//https://www.luschny.de/math/factorial/binarysplitfact.html
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -9,20 +10,62 @@ using tiii = tuple<int, int, int>;
 const int INF = 0x3f3f3f3f;
 const int MOD = 1'000'000'007;
 
+int num, per;
+
+int nminussumofbits(int v)
+{
+	long w = (long)v;
+	w -= (0xaaaaaaaa & w) >> 1;
+	w = (w & 0x33333333) + ((w >> 2) & 0x33333333);
+	w = w + (w >> 4) & 0x0f0f0f0f;
+	w += w >> 8;
+	w += w >> 16;
+	return v - (int)(w & 0xff);
+}
+
+ll partProduct(ll n, ll m)
+{
+	if (m <= (n + 1)) return (ll)n % per;
+	if (m == (n + 2)) return (ll)n * m % per;
+	ll k = (n + m) / 2;
+	if ((k & 1) != 1) k = k - 1;
+	return partProduct(n, k) * partProduct(k + 2, m) % per;
+}
+
+
+void loop(ll n, ll& p, ll& r)
+{
+	if (n <= 2) return;
+	loop(n / 2, p, r);
+	p = p * partProduct(n / 2 + 1 + ((n / 2) & 1), n - 1 + (n & 1)) % per;
+	r = r * p % per;
+}
+
+ll Factorial(int n)
+{
+	ll p = 1, r = 1;
+	loop(n, p, r);
+	int tmp = nminussumofbits(n);
+	while (tmp >= 30)
+	{
+		r *= 1 << 30;
+		r %= per;
+		tmp -= 30;
+	}
+	r <<= tmp;
+	r %= per;
+	return r;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	int n, p;
-	cin >> n >> p;
+	cin >> num >> per;
 
-	ll ans = 1;
-	for (ll i = 1; i <= n; i++)
-	{
-		ans = ans * i % p;
-	}
-	cout << ans;
+	cout << Factorial(num);
+
 
 	return 0;
 }
