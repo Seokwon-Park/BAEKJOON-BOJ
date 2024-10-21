e761204dfaa4ad6a6bd9c99d189d6bb6e7cbee64
SeokwonPark <rty3394@naver.com>
2024-10-21 10:19:52 +0900

    [Bronze II] Title: N! mod P (1), Time: 1256 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, p;
+	cin >> n >> p;
+
+	ll ans = 1;
+	for (ll i = 1; i <= n; i++)
+	{
+		ans = ans * i % p;
+	}
+	cout << ans;
+
+	return 0;
+}
