e9d78de22689192bf2af777754b7c5afffec9834
SeokwonPark <rty3394@naver.com>
2024-10-22 18:12:11 +0900

    [Bronze I] Title: 열 순서, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/16495. 열 순서/열 순서.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string s;
+	cin >> s;
+	
+	ll ans = 0;
+	ll pow = 1;
+	for (int i = 0; i < s.size(); i++)
+	{
+		int tmp = s[s.size()-i-1] - 'A'+1;
+		ans += pow*tmp;
+		pow *= 26;
+	}
+
+	cout << ans;
+
+	return 0;
+}
