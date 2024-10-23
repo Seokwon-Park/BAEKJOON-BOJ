babe3d65047fcbc66481b9b316e1ce628c23762e
SeokwonPark <rty3394@naver.com>
2024-10-23 11:02:28 +0900

    [Bronze III] Title: 오버플로우와 모듈러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15818. 오버플로우와 모듈러/오버플로우와 모듈러.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	ull ans = 1;
+	ull n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		ll k;
+		cin >> k;
+		ans *= k;
+		ans %= m;
+	}
+	cout << ans;
+	
+
+	return 0;
+}
