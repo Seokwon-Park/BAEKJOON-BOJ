b66c28f2ed81a5f17c6f3a949edfd8f7631eaf69
SeokwonPark <rty3394@naver.com>
2024-11-01 23:14:23 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n * 3; j++)
+		{
+			cout << '@';
+		}
+		for (int j = 0; j < n; j++)
+		{
+			cout << ' ';
+		}
+		for (int j = 0; j < n; j++)
+		{
+			cout << '@';
+		}
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n*3; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << '@';
+		}
+		for (int j = 0; j < n; j++)
+		{
+			cout << ' ';
+		}
+		for (int j = 0; j < n; j++)
+		{
+			cout << '@';
+		}
+		for (int j = 0; j < n; j++)
+		{
+			cout << ' ';
+		}
+		for (int j = 0; j < n; j++)
+		{
+			cout << '@';
+		}
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << '@';
+		}
+		for (int j = 0; j < n; j++)
+		{
+			cout << ' ';
+		}
+		for (int j = 0; j < n * 3; j++)
+		{
+			cout << '@';
+		}
+		cout << '\n';
+	}
+	
+	return 0;
+}
