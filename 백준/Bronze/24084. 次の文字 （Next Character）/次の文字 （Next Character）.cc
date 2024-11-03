d7324b289aa0baaa1de60bd2364f050530df41e5
SeokwonPark <rty3394@naver.com>
2024-11-03 22:36:23 +0900

    [Bronze IV] Title: 次の文字 (Next Character), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24084. 次の文字 （Next Character）/次の文字 （Next Character）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i + 1] == 'J')
+		{
+			cout << s[i] << '\n';
+		}
+	}
+
+	
+	return 0;
+}
