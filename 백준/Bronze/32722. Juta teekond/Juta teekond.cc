914cd2a04a29619e449331229cd2081cf05f08a1
SeokwonPark <rty3394@naver.com>
2024-11-19 23:01:05 +0900

    [Bronze IV] Title: Juta teekond, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32722. Juta teekond/Juta teekond.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int a, b, c;
+	cin >> a >> b >> c;
+	if ((a != 1 && a != 3) || (b != 6 && b != 8) || (c != 2 && c!= 5))
+	{
+		cout << "EI";
+	}
+	else
+	{
+		cout << "JAH";
+	}
+
+
+
+
+
+	return 0;
+}
