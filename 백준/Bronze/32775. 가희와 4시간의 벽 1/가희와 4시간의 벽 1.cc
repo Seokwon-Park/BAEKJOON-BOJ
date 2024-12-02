63809c2762a35a572c2d122325aad9dae3bd0ab6
SeokwonPark <rty3394@naver.com>
2024-12-02 23:04:34 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int sab;
+	int fab;
+	cin >> sab >> fab;
+
+	if (fab < sab)
+	{
+		cout << "flight";
+	}
+	else
+	{
+		cout << "high speed rail";
+	}
+
+
+	return 0;
+}
