5f77d6120eba6e897d608e6b4d3d5685e4e216b0
SeokwonPark <rty3394@naver.com>
2024-12-09 11:46:56 +0900

    [Silver IV] Title: Cow College, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/26975. Cow College/Cow College.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<ll> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	ll ans = 0;
+	ll tution = 0;
+	for (int i = 0; i < n; i++)
+	{
+		ll price = v[i];
+		ll res = (n - i) * price;
+		if (res > ans)
+		{
+			ans = res;
+			tution = price;
+		}
+
+	}
+	cout << ans << ' ' << tution;
+
+
+	return 0;
+}
