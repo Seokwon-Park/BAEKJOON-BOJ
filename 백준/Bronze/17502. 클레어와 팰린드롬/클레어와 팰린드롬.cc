65fa226d0c6e58ed55ba398ff299376ebb014d9e
SeokwonPark <rty3394@naver.com>
2024-12-11 00:00:22 +0900

    [Bronze II] Title: 클레어와 팰린드롬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17502. 클레어와 팰린드롬/클레어와 팰린드롬.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	for (int i = 0; i < n / 2; i++)
+	{
+		if (s[i] == '?' && s[n - i - 1] == '?')
+		{
+			s[i] = 'a';
+			s[n-i-1] = 'a';
+		}
+		else if (s[i] == '?')
+		{
+			s[i] = s[n - i - 1];
+		}
+		else if (s[n - i - 1] == '?')
+		{
+			s[n - i - 1] = s[i];
+		}
+	}
+
+	if (n % 2 && s[n/2] == '?')
+	{
+		s[n / 2] = 'a';
+	}
+
+	cout << s;
+
+
+	return 0;
+}
