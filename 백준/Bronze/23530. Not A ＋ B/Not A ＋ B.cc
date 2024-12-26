1b7cc5186d2d84cc417f7f2ae094c66341c068c6
SeokwonPark <rty3394@naver.com>
2024-12-26 23:53:16 +0900

    [Bronze III] Title: Not A + B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << 1 << '\n';
+	}
+
+	return 0;
+}
