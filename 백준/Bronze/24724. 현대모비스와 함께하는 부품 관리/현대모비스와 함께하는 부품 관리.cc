613bc4c93fbbee10c2bb9968735f0d5ee0eee315
SeokwonPark <rty3394@naver.com>
2025-01-05 23:42:48 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; i++)
+	{
+		int n;
+		cin >> n;
+		int a, b;
+		cin >> a >> b;
+		for (int j = 0; j < n; j++)
+		{
+			int u, v;
+			cin >> u >> v;
+		}
+
+		cout << "Material Management " << i << '\n';
+		cout << "Classification ---- End!\n";
+	}
+
+	return 0;
+}
