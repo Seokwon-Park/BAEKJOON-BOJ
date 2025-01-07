4f0652127cc21ed8f2dd36342372ae06ba6e13de
SeokwonPark <rty3394@naver.com>
2025-01-08 00:23:41 +0900

    [Bronze I] Title: Watering - 1, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/9285. Watering － 1/Watering － 1.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	std::cout <<
+		"aa#bb|eeeaa\n" <<
+		"acccb|fhhha\n" <<
+		"dddee|ff#bb\n" <<
+		"gfffe|gdddb\n" <<
+		"gghhh|ggccc\n" <<
+		"-----+-----\n" <<
+		"aaabb|aaabb\n" <<
+		"dcccb|d#ccb\n" <<
+		"ddeee|ddcee\n" <<
+		"ggfff|gfffe\n" <<
+		"#ghhh|gghhh\n" <<
+		"-----+-----\n" <<
+		"aaabb|aaabb\n" <<
+		"dcccb|dcccb\n" <<
+		"ddeee|dde#f\n" <<
+		"hhh#f|geeff\n" <<
+		"gggff|gghhh\n";
+
+	return 0;
+}
