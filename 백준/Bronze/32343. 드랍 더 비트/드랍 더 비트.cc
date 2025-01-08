2231a0865ca5dc77b203d9c76ea31b79c88466a0
SeokwonPark <rty3394@naver.com>
2025-01-08 23:33:30 +0900

    [Bronze I] Title: 드랍 더 비트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32343. 드랍 더 비트/드랍 더 비트.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	int a, b;
+	cin >> n >> a >> b;
+	int ans = 0;
+	int zero = 0;
+	if (a + b > n)
+	{
+		zero = a + b - n;
+	}
+	else
+	{
+		zero = n - a - b;
+	}
+	for (int i = 0; i < n - zero; i++)
+	{
+		ans <<= 1;
+		ans += 1;
+	}
+	for (int i = 0; i < zero; i++)
+	{
+		ans <<= 1;
+	}
+
+	cout << ans;
+
+
+	return 0;
+}
