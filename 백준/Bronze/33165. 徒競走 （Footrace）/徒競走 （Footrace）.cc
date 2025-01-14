084831921cd6943180b81ebcb559a461259730f7
SeokwonPark <rty3394@naver.com>
2025-01-14 23:30:28 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int t, v;
+	cin >> t >> v;
+	cout << t * v;
+
+	return 0;
+}
