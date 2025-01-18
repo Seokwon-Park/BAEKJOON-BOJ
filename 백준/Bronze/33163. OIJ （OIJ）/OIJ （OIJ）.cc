1cc1f0d3200b4350b880345af3f199dc1e25073e
SeokwonPark <rty3394@naver.com>
2025-01-18 22:04:32 +0900

    [Bronze IV] Title: OIJ (OIJ), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/33163. OIJ （OIJ）/OIJ （OIJ）.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	
+	for (auto& c : s)
+	{
+		if (c == 'J')
+			c = 'O';
+		else if (c == 'O')
+			c = 'I';
+		else
+			c = 'J';
+	}
+	cout << s;
+
+	return 0;
+}
