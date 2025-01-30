07fd1a807b7824d41a8013dd52962ad3b96e85fd
SeokwonPark <rty3394@naver.com>
2025-01-30 23:52:25 +0900

    [Bronze IV] Title: Hezardastan’s Annual Report, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33179. Hezardastan’s Annual Report/Hezardastan’s Annual Report.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		ans += x / 2;
+		ans += x % 2;
+	}
+	cout << ans;
+
+	return 0;
+}
