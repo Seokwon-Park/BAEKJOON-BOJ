5c7c66188647806b9a85441956fdd56989157fff
SeokwonPark <rty3394@naver.com>
2025-02-15 22:30:53 +0900

    [Bronze IV] Title: Штангист, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27222. Штангист/Штангист.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	vector<int> isTrain(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> isTrain[i];
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (isTrain[i])
+		{
+			if (a < b)
+			{
+				ans += b - a;
+			}
+		}
+	}
+	cout << ans;
+
+	return 0;
+}
