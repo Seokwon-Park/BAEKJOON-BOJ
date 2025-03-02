f8c71ae20eee70b5e63cb73757888d022c8c5b17
SeokwonPark <rty3394@naver.com>
2025-03-02 22:26:13 +0900

    [Bronze III] Title: 카드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11908. 카드/카드.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+ 
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	int sum = 0;
+	int mx = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		sum += x;
+		mx = max(mx, x);
+	}
+	cout << sum - mx;
+
+
+	return 0;
+}
