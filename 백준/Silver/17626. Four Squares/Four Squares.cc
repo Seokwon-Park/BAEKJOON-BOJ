99e07038739593986240834dc5503caa5a129097
SeokwonPark <rty3394@naver.com>
2025-03-12 17:37:53 +0900

    [Silver III] Title: Four Squares, Time: 8 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -2,34 +2,33 @@
 
 using namespace std;
 
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
 int dp[50005];
-// dp[i]: i를 만드는데 필요한 최소 개수의 제곱수
-// dp[1] = 1, dp[2] = min(dp[2], dp[1] + dp[2-1])
-// dp[3] = min(dp[3], dp[1] + dp[3-1])....
 
-int main() {
-	ios::sync_with_stdio(0);
+int main()
+{
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-
+	
+	dp[1] = 1;
 	int n;
 	cin >> n;
-
-	fill(dp, dp + n + 5, 0x3f3f3f3f);
-
-	for (int i = 1; i * i <= n; i++)
+	for (ll i = 2; i <= n; i++)
 	{
-		dp[i * i] = 1;
-	}
-
-	for (int i = 2; i <= n; i++)
-	{
-		for (int j = 1; j*j <= i; j++)
+		dp[i] = INF;
+		for (ll j = 1; j * j <= i; j++)
 		{
-			dp[i] = min(dp[i], dp[j*j] + dp[i - j*j]);
+			dp[i] = min(dp[i], 1 + dp[i - j * j]);
 		}
 	}
-
 	cout << dp[n];
+	
 
 	return 0;
 }
