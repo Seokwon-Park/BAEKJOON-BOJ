ae884050bdfa20a7520eafe7b7279b5141e6d142
SeokwonPark <rty3394@naver.com>
2025-03-16 16:36:10 +0900

    [Bronze III] Title: Dominos, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15236. Dominos/Dominos.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	int ans = 0;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = i; j <= n; j++)
+		{
+			ans += i + j;
+		}
+	}
+	cout << ans;
+
+	return 0;
+}
