a4e64812c6bf210b8fc446ae98681bd44fd16cd4
SeokwonPark <rty3394@naver.com>
2025-03-18 15:23:35 +0900

    [Bronze II] Title: 스물셋, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23251. 스물셋/스물셋.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		int k;
+		cin >> k;
+		cout << 23 * k << '\n';
+	}
+
+	return 0;
+}
+
+
