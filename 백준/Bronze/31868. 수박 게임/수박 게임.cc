7d9207fb986351bc985b1b955af317a42dd9d62f
SeokwonPark <rty3394@naver.com>
2025-03-26 12:00:47 +0900

    [Bronze III] Title: 수박 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	cout << k / (1 << n-1);
+
+	return 0;
+}
