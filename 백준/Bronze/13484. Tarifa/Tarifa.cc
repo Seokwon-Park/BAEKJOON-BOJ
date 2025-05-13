59f4af9203e8b20e79fa13b55e58b7c223aa7092
SeokwonPark <rty3394@naver.com>
2025-05-13 18:50:05 +0900

    [Bronze III] Title: Tarifa, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13484. Tarifa/Tarifa.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int x, n;
+	cin >> x >> n;
+	int ans = x;
+	for (int i = 0; i < n; i++)
+	{
+		int k;
+		cin >> k;
+		ans += x - k;
+	}
+	cout << ans;
+
+	return 0;
+}
