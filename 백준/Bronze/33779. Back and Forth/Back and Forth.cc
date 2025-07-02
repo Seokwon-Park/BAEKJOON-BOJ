45bc35a622db621481eae61215631abcfe6d7057
SeokwonPark <rty3394@naver.com>
2025-07-02 22:56:50 +0900

    [Bronze III] Title: Back and Forth, Time: 0 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Bronze/33779. Back and Forth/Back and Forth.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size() / 2; i++)
+	{
+		if (s[i] != s[s.size() - 1 - i])
+		{
+			cout << "boop";
+			return 0;
+		}
+	}
+	cout << "beep";
+
+
+	return 0;
+}
