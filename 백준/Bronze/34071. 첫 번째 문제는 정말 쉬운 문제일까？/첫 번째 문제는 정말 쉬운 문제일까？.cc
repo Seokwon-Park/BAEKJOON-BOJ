36216b00dcf7bafa3bada241edf746dc77a0d501
SeokwonPark <rty3394@naver.com>
2025-07-22 22:58:45 +0900

    [Bronze IV] Title: 첫 번째 문제는 정말 쉬운 문제일까?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34071. 첫 번째 문제는 정말 쉬운 문제일까？/첫 번째 문제는 정말 쉬운 문제일까？.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'000;
+
+int b[5][5];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	int mn = INF;
+	int mx = 0;
+	for (int& i : v)
+	{
+		cin >> i;
+		mn = min(i, mn);
+		mx = max(i, mx);
+	}
+	if (v[0] == mn) cout << "ez";
+	else if (v[0] == mx) cout << "hard";
+	else cout << "?";
+	
+
+
+	return 0;
+}
