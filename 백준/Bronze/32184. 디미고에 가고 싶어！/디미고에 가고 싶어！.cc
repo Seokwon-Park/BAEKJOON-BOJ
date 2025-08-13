836816de489912537b73b68cd4a7f828327586ea
SeokwonPark <rty3394@naver.com>
2025-08-13 21:28:07 +0900

    [Bronze III] Title: 디미고에 가고 싶어!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32184. 디미고에 가고 싶어！/디미고에 가고 싶어！.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int a, b;
+	cin >> a >> b;
+	if (a % 2==0)
+	{
+		a--;
+	}
+	if (b % 2)
+	{
+		b++;
+	}
+
+	cout << (b - a+1) / 2;
+	
+	return 0;
+}
