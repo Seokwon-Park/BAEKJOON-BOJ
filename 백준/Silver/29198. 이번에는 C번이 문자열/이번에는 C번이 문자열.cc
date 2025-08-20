5effbe25421e2ce413e9ed1a4c3a9d94714880d3
SeokwonPark <rty3394@naver.com>
2025-08-20 15:31:55 +0900

    [Silver III] Title: 이번에는 C번이 문자열, Time: 4 ms, Memory: 2448 KB, Score: 35 point -BaekjoonHub

---
+++ 백준/Silver/29198. 이번에는 C번이 문자열/이번에는 C번이 문자열.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m, k;
+	cin >> n >> m >> k;
+	
+	vector<string> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+
+	string ans = "";
+	for (int i = 0; i < k; i++)
+	{
+		ans += v[i];
+	}
+
+	sort(ans.begin(), ans.end());
+
+	cout << ans;
+
+	return 0;
+}
