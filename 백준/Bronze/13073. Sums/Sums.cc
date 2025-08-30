a76918d5ef28f9876c42b74dfd65200e99f5ec9c
SeokwonPark <rty3394@naver.com>
2025-08-30 23:56:44 +0900

    [Bronze III] Title: Sums, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13073. Sums/Sums.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		ll n;
+		cin >> n;
+		cout << n * (n + 1) / 2 << ' ' << n * n << ' ' << n * (n + 1) << '\n';
+	}
+
+	return 0;
+}
