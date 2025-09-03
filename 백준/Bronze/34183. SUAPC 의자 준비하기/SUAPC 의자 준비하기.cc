3e8532034adf06988ff25da62edcee06ab1304a1
SeokwonPark <rty3394@naver.com>
2025-09-03 18:24:04 +0900

    [Bronze IV] Title: SUAPC 의자 준비하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34183. SUAPC 의자 준비하기/SUAPC 의자 준비하기.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n, m, a, b;
+	cin >> n >> m >> a >> b;
+	int need = n * 3;
+	if (need <= m)
+	{
+		cout << 0;
+	}
+	else
+	{
+		cout << (need - m) * a + b;
+	}
+
+	return 0;
+}
