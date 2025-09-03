44adb11293dd4b95b1d9dc9c63281b1315680ff7
SeokwonPark <rty3394@naver.com>
2025-09-03 18:56:33 +0900

    [Bronze I] Title: Fractions are better when continued, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32437. Fractions are better when continued/Fractions are better when continued.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	int a = 1;
+	int b = 1;
+	for (int i = 0; i < n; i++)
+	{
+		a += b;
+		swap(a, b);
+	}
+	cout << a;
+
+	return 0;
+}
