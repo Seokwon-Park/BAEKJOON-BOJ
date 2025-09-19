c6e7a4224b171705cafdb245683cafab7299072b
SeokwonPark <rty3394@naver.com>
2025-09-20 00:59:25 +0900

    [Bronze V] Title: Flatter Land, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34424. Flatter Land/Flatter Land.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, x;
+	cin >> n >> x;
+	cout << (n - 1) * x;
+	return 0;
+}
