0909699aa46c8c7a7fbf31832451029de7d5739f
SeokwonPark <rty3394@naver.com>
2025-09-20 01:00:23 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m, t;
+	cin >> n >> m >> t;
+	cout << n * 2;
+	
+	return 0;
+}
