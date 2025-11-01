97e8b89a08dfd224f24e0b7addcaa7b0ee691256
SeokwonPark <rty3394@naver.com>
2025-11-01 22:45:39 +0900

    [Gold V] Title: 극한직업 - 영양사 선생님, Time: 52 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Gold/32945. 극한직업 － 영양사 선생님/극한직업 － 영양사 선생님.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	int cur = 1;
+	for (int i = 0; i < n; i++)
+	{
+		if (v[i] >= cur)
+		{
+			cur++;
+		}
+	}
+
+	cout << cur-1;
+
+
+
+	return 0;
+}
