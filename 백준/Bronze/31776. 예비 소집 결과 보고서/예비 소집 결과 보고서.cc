8e7461af335141de900555f8fbcbc77ee21d1d31
SeokwonPark <rty3394@naver.com>
2025-11-08 23:56:11 +0900

    [Bronze III] Title: 예비 소집 결과 보고서, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31776. 예비 소집 결과 보고서/예비 소집 결과 보고서.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	int ans = 0;
+	while (n--)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		if(a >-1 && b == -1 && c == -1)
+		{
+			ans++;
+		}
+		else if (a > -1 && b > -1 && c == -1)
+		{
+			if (a <= b)
+			{
+				ans++;
+			}
+		}
+		else if (a > -1 && b > -1 && c > -1)
+		{
+			if (a <= b && b <= c)
+			{
+				ans++;
+			}
+		}
+	}
+	cout << ans;
+
+	return 0;
+}
