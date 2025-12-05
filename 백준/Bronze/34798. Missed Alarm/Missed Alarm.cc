cd917db579740948b1b5b9b2dd954d270df97d89
SeokwonPark <rty3394@naver.com>
2025-12-05 23:49:08 +0900

    [Bronze IV] Title: Missed Alarm, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34798. Missed Alarm/Missed Alarm.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+ 
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, b, c, d;
+	char e;
+	cin >> a >> e >> b >> c >> e >> d;
+	int x = a * 60 + b;
+	int y = c * 60 + d;
+
+	cout << (x < y ? "YES" : "NO");
+
+
+	return 0;
+}
