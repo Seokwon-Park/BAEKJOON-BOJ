f4b4432859fcb24fa46303e5569c90bf4506dbc7
SeokwonPark <rty3394@naver.com>
2025-12-17 18:33:19 +0900

    [Silver I] Title: 균등분포와 정규분포, Time: 112 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/26523. 균등분포와 정규분포/균등분포와 정규분포.cc
@@ -0,0 +1,42 @@
+// 에디토리얼을 참고함
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	for (int i = 0; i < 100; i++)
+	{
+		int cnt = 0;
+		for (int j = 0; j < 5000; j++)
+		{
+			double x;
+			cin >> x;
+			if (x <= 0.05) cnt++;
+		}
+		if (cnt >= 180)
+		{
+			cout << 'A';
+		}
+		else
+		{
+			cout << 'B';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
