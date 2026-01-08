b9745296913fe13e2e6264494f25bd31a0e72af9
SeokwonPark <rty3394@naver.com>
2026-01-08 17:10:30 +0900

    [Gold V] Title: 두 부분 문자열, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

---
+++ 백준/Gold/11056. 두 부분 문자열/두 부분 문자열.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int dp[1005][1005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string a, b;
+	cin >> a >> b;
+
+	for (int i = 1; i <= a.size(); i++)
+	{
+		for (int j = 1; j <= b.size(); j++)
+		{
+			if (a[i - 1] == b[j - 1])
+			{
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else
+			{
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+
+	cout << a.size()+b.size()- dp[a.size()][b.size()];
+
+
+	return 0;
+}
