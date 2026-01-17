905fd1f8c156f6762db95211b51ced2924ffc3a0
SeokwonPark <rty3394@naver.com>
2026-01-17 23:58:44 +0900

    [Bronze III] Title: Disposable Cup, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13129. Disposable Cup/Disposable Cup.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using hashmap = unordered_map<key, value>;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int a, b, n;
+	cin >> a >> b >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << b * (i+1) + a * n << ' ';
+	}
+
+	return 0;
+}
