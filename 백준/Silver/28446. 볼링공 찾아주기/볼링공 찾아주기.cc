de6c3b526c3b93f0cb41a73c91d7fd91a7b614f8
SeokwonPark <rty3394@naver.com>
2026-02-14 14:54:27 +0900

    [Silver IV] Title: 볼링공 찾아주기, Time: 192 ms, Memory: 12692 KB -BaekjoonHub

---
+++ 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	Hash<int, int> m;
+	for (int i = 0; i < n; i++)
+	{
+		int q, a, b;
+		cin >> q;
+		if (q == 1)
+		{
+			cin >> a >> b;
+			m[b] = a;
+		}
+		else
+		{
+			cin >> b;
+			cout << m[b] << '\n';
+		}
+	}
+
+	return 0;
+}
