d4c6f9d62555255ce028153917830d582c1756a3
SeokwonPark <rty3394@naver.com>
2026-02-17 15:17:11 +0900

    [Platinum IV] Title: 광고, Time: 8 ms, Memory: 8056 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+vector<int> failure(string s)
+{
+	int j = 0;
+	vector<int> f(s.size());
+	for (int i = 1; i < s.size(); i++)
+	{
+		while (j > 0 && s[i] != s[j]) j = f[j - 1];
+		if (s[i] == s[j]) f[i] = ++j;
+	}
+	return f;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+
+	vector<int> f = failure(s);
+
+	cout << n - f[s.size() - 1];
+
+	return 0;
+}
