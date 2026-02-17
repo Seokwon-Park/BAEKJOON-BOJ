112d256142011c6039a3296c12f7ddea5e101a27
SeokwonPark <rty3394@naver.com>
2026-02-17 16:05:29 +0900

    [Bronze III] Title: Walk Like an Egyptian, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4922. Walk Like an Egyptian/Walk Like an Egyptian.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n;
+	while (1)
+	{
+		cin >> n;
+		if (!n) break;
+		int cur = 1;
+		for (int i = 1; i < n; i++)
+		{
+			cur += i * 2;
+		}
+		cout << n << " => " << cur << '\n';
+	}
+
+
+	return 0;
+}
