beee50d13846e00548163fbe1cf33bd2a6607e7b
SeokwonPark <rty3394@naver.com>
2026-03-14 14:50:02 +0900

    [Gold II] Title: 분수 뺄셈, Time: 852 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/4782. 분수 뺄셈/분수 뺄셈.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	ll b, n;
+	while (cin >> b >> n)
+	{
+		vector<pair<ll, ll>> v;
+		if (b == 0 && n == 0)break;
+		for (ll k = -n; b * k * k / (n * n) <= b; k++)
+		{
+			if (n + k == 0 || (n + k == n && b - b * k * k / (n * n) == b)) continue;
+			if (b * k * k % (n * n) == 0)
+			{
+				v.push_back({ b - b * k * k / (n * n), n + k });
+			}
+		}
+
+		sort(v.begin(), v.end(), [](auto& a, auto& b)
+			{
+				return (double)a.first /a.second < (double)b.first / b.second;
+			});
+
+		for (auto [a, b] : v)
+		{
+			cout << a << '/' << b << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
