4d6192188c8f974ef7b5d37a76d0d220e91a0f0c
SeokwonPark <rty3394@naver.com>
2026-03-25 12:03:18 +0900

    [Silver III] Title: This Can’t Go On Forever, Time: 964 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4473. This Can’t Go On Forever/This Can’t Go On Forever.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	ll n;
+	while (cin >> n)
+	{
+		if (!n) break;
+		ll a = 1;
+		ll b = 2;
+		int len = 2;
+		while (a % n != 0 || b % n != 1)
+		{
+			ll nxt = (a + b);
+			a = b%n;
+			b = nxt%n;
+			len++;
+		}
+		cout << n << ' ' << len << '\n';
+	}
+
+
+
+	return 0;
+}
