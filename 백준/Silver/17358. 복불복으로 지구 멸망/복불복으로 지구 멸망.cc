24e514626fdf4ab13d23ff98381688017edce10d
SeokwonPark <rty3394@naver.com>
2026-04-07 17:38:58 +0900

    [Silver IV] Title: 복불복으로 지구 멸망, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17358. 복불복으로 지구 멸망/복불복으로 지구 멸망.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+//const int MOD = 987'654'321;
+template <typename key, typename value>
+using Hash = unordered_map<key, value>;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+ 
+	ll n;
+	cin >> n;
+
+	ll ans = 1;
+	for (ll i = 1; i<n;i+=2)
+	{
+		ans *= i;
+		ans %= MOD;
+	}
+	cout << ans;
+
+	return 0;
+}
